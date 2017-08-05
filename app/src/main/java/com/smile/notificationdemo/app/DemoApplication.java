c86f4edd13ceed80b13e7ba32a867c187c467d9a
EasyToForget <zhiye.wei@gmail.com>
2017-08-05 18:26:49 +0800

    add README.md

--- app/src/main/java/com/smile/notificationdemo/app/DemoApplication.java
+++ app/src/main/java/com/smile/notificationdemo/app/DemoApplication.java
@@ -104,27 +104,22 @@ public class DemoApplication extends Application {
 
             @Override
             public void onActivityStarted(Activity activity) {
-
             }
 
             @Override
             public void onActivityResumed(Activity activity) {
-
             }
 
             @Override
             public void onActivityPaused(Activity activity) {
-
             }
 
             @Override
             public void onActivityStopped(Activity activity) {
-
             }
 
             @Override
             public void onActivitySaveInstanceState(Activity activity, Bundle outState) {
-
             }
 
             @Override
